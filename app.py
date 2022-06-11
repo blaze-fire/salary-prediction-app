3248923836fd96120329d45af3dcb3d43a537465
blaze-fire <krishans290@gmail.com>
2022-06-11 16:11:46 +0530

    deploy with databse

---
+++ app.py
@@ -0,0 +1,116 @@
+from flask import Flask, jsonify, request, render_template
+import numpy as np
+import pickle
+import pandas as pd
+from utils.nlp_utils import Word2VecVectorizer
+from utils.data_preprocessing import Preprocess
+from gensim.models import KeyedVectors
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Import for Migrations
+from flask_migrate import Migrate, migrate
+ 
+
+
+
+app = Flask(__name__)
+
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+
+# Settings for migrations
+migrate = Migrate(app, db)
+# Models
+
+class Profile(db.Model):
+    # Id : Field which stores unique id for every row in
+
+    id = db.Column(db.Integer, primary_key=True)
+    Job_position = db.Column(db.String(100), unique=False, nullable=False)
+    Company = db.Column(db.String(100), unique=False, nullable=False) 
+    Location = db.Column(db.String(100), unique=False, nullable=False) 
+    requirements = db.Column(db.String(3000), unique=False, nullable=False) 
+    rating = db.Column(db.Integer)
+    experience = db.Column(db.String(3000), unique=False, nullable=False) 
+    posting_frequency = db.Column(db.Integer)
+    
+    # repr method represents how one object of this datatable
+    # will look like
+    def __repr__(self):
+        return f"Company : {self.Company}, Job Profile: {self.Job_position}"
+
+
+
+
+
+
+
+
+#load glove embeddings
+filename = 'utils/glove_50d.gs'
+
+model = KeyedVectors.load(filename, mmap='r')
+
+def glove_embedded(X, col,train_data):
+  vectorizer = Word2VecVectorizer(model)
+  X_embed = vectorizer.fit_transform(X[col].apply(str))
+  train_data = np.concatenate((X_embed, train_data), axis=1)
+  
+  return train_data
+
+
+#load model
+xgb_model = pickle.load(open('models/xgb_model.sav', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# function to render index page
+@app.route('/show')
+def index():
+    profiles = Profile.query.all()
+    return render_template('show.html', profiles=profiles)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():   
+     
+    x_in  = list(request.form.values())
+    
+    columns = ['Job_position', 'Company', 'Location', 'requirements', 'rating', 'experience', 'posting_frequency']
+
+    input_df = pd.DataFrame(columns = columns)
+
+    for j in range(len(x_in)):
+        input_df.loc[0, columns[j]] = x_in[j]
+    
+    print(input_df['Job_position'].values[0])
+
+    p = Profile(Job_position = input_df['Job_position'].values[0], Company=input_df['Company'].values[0], 
+                Location=input_df['Location'].values[0], requirements = input_df['requirements'].values[0],
+                rating = input_df['rating'].values[0], experience = input_df['experience'].values[0], 
+                posting_frequency=input_df['posting_frequency'].values[0])
+
+    db.session.add(p)
+    db.session.commit()
+
+    # input_df = Preprocess()(input_df)
+
+    # train_data = input_df.select_dtypes(exclude='object').values
+    
+    # for col in input_df.select_dtypes(include='object').columns:
+    #     train_data = glove_embedded(input_df, col, train_data)
+
+    # pred = xgb_model.predict(train_data)
+    # prediction = np.round(np.exp(pred), 2)
+    prediction = 0
+    return render_template('index.html', prediction_text='Your predicted annual salary is {}'.format(prediction))
+
+if __name__ == '__main__':
+ app.run(debug=True)
