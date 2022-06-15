32748ae3109ff33627c7de1c99e1384386621164
blaze-fire <krishans290@gmail.com>
2022-06-15 18:18:06 +0530

    added two plots and also labelled them

--- app.py
+++ app.py
@@ -4,23 +4,33 @@ import pandas as pd
 from utils.data_preprocessing import Preprocess
 from flask_sqlalchemy import SQLAlchemy
 from pickle import load
+import matplotlib.pyplot as plt
 from scipy.sparse import hstack
 import os
 
+import io
+import base64
+
+import re
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import seaborn as sns
+
+
 # Import for Migrations
 from flask_migrate import Migrate, migrate
- 
+
 
 app = Flask(__name__)
 
 # adding configuration for using a sqlite database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+# uri = os.getenv("DATABASE_URL")  # or other relevant config var
+# if uri and uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
+# app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 
 # Creating an SQLAlchemy instance
@@ -30,17 +40,19 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Models
+
+
 class Profile(db.Model):
     # Id : Field which stores unique id for every row in
 
     id = db.Column(db.Integer, primary_key=True)
     Job_position = db.Column(db.String(100), unique=False, nullable=False)
-    Company = db.Column(db.String(100), unique=False, nullable=False) 
-    Location = db.Column(db.String(100), unique=False, nullable=False) 
-    requirements = db.Column(db.String(3000), unique=False, nullable=False) 
+    Company = db.Column(db.String(100), unique=False, nullable=False)
+    Location = db.Column(db.String(100), unique=False, nullable=False)
+    requirements = db.Column(db.String(10000), unique=False, nullable=False)
     rating = db.Column(db.Float, unique=False)
-    experience = db.Column(db.String(3000), unique=False, nullable=False) 
-    
+    experience = db.Column(db.String(10000), unique=False, nullable=False)
+
     # repr method represents how one object of this datatable
     # will look like
     def __repr__(self):
@@ -52,54 +64,143 @@ def home():
     return render_template('index.html')
 
 # function to render index page
+
+
 @app.route('/show')
 def index():
     profiles = Profile.query.all()
     return render_template('show.html', profiles=profiles)
 
 
-@app.route('/predict', methods=['POST'])
-def predict():   
-     
-    x_in  = list(request.form.values())
-    
-    columns = ['Job_position', 'Company', 'Location', 'requirements', 'rating', 'experience']
+def plotView(input_df):
 
-    input_df = pd.DataFrame(columns = columns)
+    # predicted salary from '/predict'
+    predictedSal = input_df['avg_yearly_sal'].values[0]
+
+    df = pd.read_csv(r'data/data_for_eda.csv')
+    df = df[df['avg_yearly_sal'] > 0]
+    sal = df['avg_yearly_sal']
+    sal.reset_index(drop=True, inplace=True)
+    sal = sal.append(
+        pd.Series({'avg_yearly_sal': predictedSal}), ignore_index=True)
+
+    # Plot Salary Distribution
+    fig = Figure(figsize=(10, 15))
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax1.set_title("You vs Others")
+    ax1.set_xlabel("Annual Average Salary (in crores)")
+    ax1.set_ylabel("Count of Number of People")
+    ax1.grid()
+
+    # Generate bins with interval of 100000
+    bins = np.arange(10000, 10000000, 100000)
+    sns.histplot(ax=ax1, x=sal, bins=bins)
+
+    # To get the first index of the bin where it is given than the current predicted salary
+    idx = np.nonzero(bins > predictedSal)[0][0]
+    ax1.patches[idx].set_facecolor('salmon')
+    ax1.annotate(format(f'Rs. {predictedSal}'),
+                     (ax1.patches[idx].get_x() + ax1.patches[idx].get_width() / 2., ax1.patches[idx].get_height()),
+                     ha='center', va='center',
+                     xytext=(0, 100),arrowprops=dict(arrowstyle="->", connectionstyle="arc3"), textcoords='offset points')
+
+    # ------------------------------------------------------------------------------------------------------------------------------
+
+    # Skills vs salary plot (Bassically plot the average salary for the skills mentioned in the JD)
+    text = input_df['requirements'].values[0]
+    with open('utils/skills.txt') as f:
+        skills = f.read()
+    f.close()
+
+    skills = skills.split(',')
+    input_skills = list(set(re.findall(r"(?=(\b" + '\\b|\\b'.join(skills) + r"\b))", text)))
+
+    df_skills = list(df['Job_position'].unique())
+    input_skills = list(set(input_skills) & set(df_skills))
+
+    # To build the dataframe of average salaries of skills mentioned in the requirements section
+    plot_df = pd.DataFrame(columns=['skill', 'avg_yearly_sal'])
+    for i in input_skills:
+        avg_sal = df[df['Job_position'] == i]['avg_yearly_sal'].values.mean()
+        plot_df = plot_df.append(
+                                pd.Series({'skill': i, 'avg_yearly_sal': avg_sal}), 
+                                ignore_index=True
+                                )
+
+    plot_df.sort_values(by='avg_yearly_sal', inplace=True)
+
+    ax2 = fig.add_subplot(2, 1, 2)
+    ax2.set_title("Average pay with these skills")
+    ax2.grid()
+    ax2 = sns.barplot(y='avg_yearly_sal', x='skill', data=plot_df, ax=ax2)
+    for p in ax2.patches:
+        ax2.annotate(format(p.get_height(), '.1f'),
+                     (p.get_x() + p.get_width() / 2., p.get_height()),
+                     ha='center', va='center',
+                     xytext=(0, 9),
+                     textcoords='offset points')
+    ax2.set_ylabel("Annual Average Salary")
+
+    # ------------------------------------------------------------------------------------------------------------------------------
+
+    # Convert plot to PNG image
+    pngImage = io.BytesIO()
+    FigureCanvas(fig).print_png(pngImage)
+
+    # Encode PNG image to base64 string
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+    return render_template("image.html", image=pngImageB64String, prediction=predictedSal)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    x_in = list(request.form.values())
+
+    columns = ['Job_position', 'Company', 'Location',
+               'requirements', 'rating', 'experience']
+
+    input_df = pd.DataFrame(columns=columns)
 
     for j in range(len(x_in)):
-        input_df.loc[0, columns[j]] = x_in[j]    
-            
+        input_df.loc[0, columns[j]] = x_in[j]
+
     input_df['rating'] = pd.to_numeric(input_df['rating'])
 
-    p = Profile(Job_position = input_df['Job_position'].values[0], Company=input_df['Company'].values[0], 
-                Location=input_df['Location'].values[0], requirements = input_df['requirements'].values[0],
-                rating = float(input_df['rating'].values[0]), experience = input_df['experience'].values[0], 
+    p = Profile(Job_position=input_df['Job_position'].values[0],
+                Company=input_df['Company'].values[0],
+                Location=input_df['Location'].values[0],
+                requirements=input_df['requirements'].values[0],
+                rating=float(input_df['rating'].values[0]),
+                experience=input_df['experience'].values[0],
                 )
 
-
-    db.session.add(p)
-    db.session.commit()
-
+    # db.session.add(p)
+    # db.session.commit()
 
     # load the model
     model = load(open('models/model.pkl', 'rb'))
-    
+
     # load the scaler
     transformer = load(open('models/scaler.pkl', 'rb'))
 
-    input_df = Preprocess()(input_df)
-    text_data = transformer.transform(input_df)
-    num_cols = list(input_df.select_dtypes(exclude='object').columns)
+    processed_df = Preprocess()(input_df)
+    text_data = transformer.transform(processed_df)
+    num_cols = list(processed_df.select_dtypes(exclude='object').columns)
 
-    train_stack = hstack((input_df[num_cols].values, text_data))
-    
+    train_stack = hstack((processed_df[num_cols].values, text_data))
+
     pred = model.predict(train_stack)
+
+    print(pred)
+    
     prediction = np.round(pred, 2)
 
-    return render_template('index.html', prediction_text='Your predicted annual salary is {}'.format(prediction[0]))
-
+    input_df['avg_yearly_sal'] = prediction[0]
+    return plotView(input_df)
 
 
 if __name__ == '__main__':
- app.run(debug=True)
+    app.run(debug=True)
